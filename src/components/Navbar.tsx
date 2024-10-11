270cf623f2666f8632082589fbced0f3a87c344b
HaiderNadaf <haidernadaf67@gmail.com>
2024-10-10 23:45:19 -0700

    changed image path

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
       </div>
       <div className='text-2xl font-bold flex flex-row'>
         <Link href="/" className='underline'>PIZZA POINT</Link>
-        <Image src="/pizza.png" width={30} height={30} alt="" />
+        <Image src="/pizza.jpg" width={30} height={30} alt="" />
         
       </div>
       <div className='md:hidden'>
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -7,13 +7,13 @@ const data = [
   {
   id: 1,
   title: "always fresh & always crispy & always hot",
-  image: "/slide1.png",
+  image: "/slide1.jpg",
   },
   {
   id: 2,
  title: "we deliver your order wherever you are in NY",
 
-  image: "/slide2.png",
+  image: "/slide2.jpg",
   },
   {
   id: 3,
