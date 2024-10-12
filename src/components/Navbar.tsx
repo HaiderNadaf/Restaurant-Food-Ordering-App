ce8af6acbe787ac6a8e1cf2147095dca73e0a165
HaiderNadaf <haidernadaf67@gmail.com>
2024-10-12 00:15:02 -0700

    changed in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,32 +5,44 @@ import CartIcon from './CartIcon'
 import Image from 'next/image'
 
 const Navbar = () => {
-  const user = false
+  const user = false  // Simulating an unauthenticated user. Update this logic as needed.
+
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* Links for desktop */}
       <div className='hidden md:flex gap-4'>
         <Link href="/" className='hover:font-bold hover:text-xl'>HomePage</Link>
         <Link href="/menu" className='hover:font-bold hover:text-xl'>Type's</Link>
-        <Link href="/menu/Order" className='hover:font-bold hover:text-xl'>menu</Link>
+        <Link href="/menu/Order" className='hover:font-bold hover:text-xl'>Menu</Link>
       </div>
+
+      {/* Logo section */}
       <div className='text-2xl font-bold flex flex-row'>
         <Link href="/" className='underline'>PIZZA POINT</Link>
-        <Image src="/pizza.jpg" width={30} height={30} alt="" />
-        
+        <Image src="/pizza.jpg" width={30} height={30} alt="Pizza Logo" />
       </div>
+
+      {/* Mobile Menu */}
       <div className='md:hidden'>
-       <Menu/>
+        <Menu />
       </div>
+
+      {/* Contact & Cart for desktop */}
       <div className='hidden md:flex gap-4 items-center'>
         <div className='flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md'>
-          <Image src="/phone.png" alt='' width={20} height={20}/>
+          <Image src="/phone.png" alt="Phone Icon" width={20} height={20} />
           <span>9900768505</span>
         </div>
-     
-     <CartIcon/>
-     {!user ?<Link href="/order" className='hover:font-bold hover:text-xl'>order</Link> :
-        <Link href="/login" className='hover:font-bold hover:text-xl'>login</Link>}
-         <Link href="/login" className='hover:font-bold hover:text-xl'>login</Link>
+
+        {/* Cart Icon */}
+        <CartIcon />
+
+        {/* Conditional rendering for Order/Login based on user authentication */}
+        {!user ? (
+          <Link href="/order" className='hover:font-bold hover:text-xl'>Order</Link>
+        ) : (
+          <Link href="/login" className='hover:font-bold hover:text-xl'>Login</Link>
+        )}
       </div>
     </div>
   )
